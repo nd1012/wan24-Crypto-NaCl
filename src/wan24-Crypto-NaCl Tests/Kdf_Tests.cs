f208a5ab7a7a20f06a92a2300b44180a4e3a13fa
nd <nd1012@users.noreply.github.com>
2024-09-09 10:05:38 +0700

    Dev (#19)
    + Updated references

--- src/wan24-Crypto-NaCl Tests/Kdf_Tests.cs
+++ src/wan24-Crypto-NaCl Tests/Kdf_Tests.cs
@@ -1,9 +1,10 @@
 ﻿using wan24.Crypto.Tests;
+using wan24.Tests;
 
 namespace wan24_Crypto_NaCl_Tests
 {
     [TestClass]
-    public class Kdf_Tests
+    public class Kdf_Tests : TestBase
     {
         [TestMethod]
         public void All_Tests() => KdfTests.TestAllAlgorithms();
